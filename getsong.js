0b5a80a31be9888eda29d7ebc4a3dc578d6f7210
anubhav badak <anubhavbadak@gmail.com>
2021-04-11 06:30:38 +0530

    song downloader without an html page

---
+++ getsong.js
@@ -0,0 +1,21 @@
+var http = require("http");
+const fs = require("fs");
+let url = require("url");
+
+
+http.createServer(function(req,res){
+    if(req.url === "/form"){
+        console.log("in request");
+
+        res.writeHead(200,{"Content-Type":"text/html"});
+         fs.createReadStream("./public/songDownloader.html", "UTF-8").pipe(res);
+
+         //res.end(songDownloader.html);
+         console.log("html page loaded");
+
+         if(req.method === "GET"){
+             var q = url.parse(req.url, true);
+             console.log(q);
+         }
+}
+}).listen(3000);
---
+++ songDownloader.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+const fs = require('puppeteer');
+let tab;
+//let songname = "the plan";
+const prompt = require('prompt-sync')();
+
+const songname = prompt('Enter songName ');
+
+(async function(){
+    let browser = await puppeteer.launch({
+        headless:false,
+        defaultViewport:null,
+        args: ["--start-maximized"],
+    })
+    let allpages = await browser.pages();
+    tab = allpages[0];
+    await tab.goto("https://www.youtube.com");
+
+    await tab.waitForSelector(`input[id="search"]`);
+    await tab.type(`input[id="search"]`,songname);
+    await tab.click(`button[id="search-icon-legacy"]`);
+    await tab.waitForSelector(`a[id="video-title"]`);
+
+    let song_aTag = await tab.$(`a[id="video-title"]`);
+    console.log(song_aTag);
+    let songUrl = await tab.evaluate(function(elem){
+        return elem.getAttribute("href");
+    },song_aTag);
+    songUrl = "https://www.youtube.com" + songUrl;
+    console.log(songUrl);
+
+    let newTab = await browser.newPage();
+    await newTab.goto("https://ytmp3.cc/youtube-to-mp3/");
+    await newTab.waitForSelector(`input[id="input"]`);
+    await newTab.type(`input[id="input"]`,songUrl);
+    await newTab.waitForTimeout(3000);
+    await newTab.click(`input[id="submit"]`);
+    await newTab.waitForSelector(`div[id="buttons"] a`);
+    await newTab.waitForTimeout(3000);
+    await newTab.click(`div[id="buttons"] a`);
+    // let download_link = await newTab.$(`div[id="buttons"] a`);
+    // console.log(download_link);
+
+
+})();
